e8cf6b535c9a2691a66879bcd694c73402c61591
hyunji_jeon <hyunji_jeon@tmax.co.kr>
2021-07-28 18:05:29 +0900

    nothing

--- my-react-app/src/test/example01/child.tsx
+++ my-react-app/src/test/example01/child.tsx
@@ -17,30 +17,6 @@ export class ChildComponent extends React.Component<IProps, IState> {
         };
     }
 
-    // props으로 부터 state가 업데이트 되야하는 경우 (== useEffect), 오용 될 수 있어서 16.3 버전부터 deprecated 됨
-    // state에 이전의 prop의 id를 기록해서 props이 변경된 경우에 state 업데이트
-    // componentWillReceiveProps(nextProps: Props) {
-    //     if (nextProps.query.id !== this.state.previousId) {
-    //         this.setState({ ...this.state, previousId: nextProps.id});
-    //     }
-    // }
-
-    // state, prop 변경, 부모 render 시점에 호출됨
-    // previousId enables to access the previous props value in the same way as is done in componentWillReceiveProps
-    // static getDerivedStateFromProps(props: IProps, state: IState) {
-    //     if (props.query.id !== state.previousId) { 
-    //         return {
-    //             ...state,
-    //             query: {
-    //                 id: props.query.id
-    //             },
-    //             previousId: props.query.id
-    //         };
-    //     } else {//state가 변경된 경우
-    //         return state;
-    //     }
-    // }
-
     onChangeId(ev: React.ChangeEvent): void {
         const elm = ev.target as HTMLInputElement;
         this.setState({
